c3d4761c68207fc65398991afee788b056551367
plackyhacker <john.tear@mac.com>
2021-12-24 11:57:20 +0000

    Added Native class for P/Invoke stuff.

---
+++ peruns-fart/peruns-fart/Native.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PerunsFart
+{
+    class Native
+    {
+    }
+}
--- peruns-fart/peruns-fart/Program.cs
+++ peruns-fart/peruns-fart/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
-namespace peruns_fart
+using static PerunsFart.Native;
+
+namespace PerunsFart
 {
     class Program
     {
