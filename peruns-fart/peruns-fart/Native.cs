cb51c6dca18707941336355d1ea6603d96bcd33b
plackyhacker <john.tear@mac.com>
2021-12-24 15:28:50 +0000

    Implemented GetModuleSize()

--- peruns-fart/peruns-fart/Native.cs
+++ peruns-fart/peruns-fart/Native.cs
@@ -10,6 +10,9 @@ namespace PerunsFart
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandles, CreateProcessFlags dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
 
+        [DllImport("kernel32.dll", EntryPoint = "GetModuleHandleW", SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string moduleName);
+
         [StructLayout(LayoutKind.Sequential)]
         public struct SECURITY_ATTRIBUTES
         {
@@ -69,6 +72,228 @@ namespace PerunsFart
             DETACHED_PROCESS = 0x00000008,
             EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
             INHERIT_PARENT_AFFINITY = 0x00010000
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct IMAGE_DOS_HEADER
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            public char[] e_magic;       // Magic number
+            public UInt16 e_cblp;    // Bytes on last page of file
+            public UInt16 e_cp;      // Pages in file
+            public UInt16 e_crlc;    // Relocations
+            public UInt16 e_cparhdr;     // Size of header in paragraphs
+            public UInt16 e_minalloc;    // Minimum extra paragraphs needed
+            public UInt16 e_maxalloc;    // Maximum extra paragraphs needed
+            public UInt16 e_ss;      // Initial (relative) SS value
+            public UInt16 e_sp;      // Initial SP value
+            public UInt16 e_csum;    // Checksum
+            public UInt16 e_ip;      // Initial IP value
+            public UInt16 e_cs;      // Initial (relative) CS value
+            public UInt16 e_lfarlc;     // File address of relocation table
+            public UInt16 e_ovno;    // Overlay number
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public UInt16[] e_res1;    // Reserved words
+            public UInt16 e_oemid;       // OEM identifier (for e_oeminfo)
+            public UInt16 e_oeminfo;     // OEM information; e_oemid specific
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+            public UInt16[] e_res2;    // Reserved words
+            public Int32 e_lfanew;      // File address of new exe header
+
+            private string _e_magic
+            {
+                get { return new string(e_magic); }
+            }
+
+            public bool isValid
+            {
+                get { return _e_magic == "MZ"; }
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct IMAGE_FILE_HEADER
+        {
+            public UInt16 Machine;
+            public UInt16 NumberOfSections;
+            public UInt32 TimeDateStamp;
+            public UInt32 PointerToSymbolTable;
+            public UInt32 NumberOfSymbols;
+            public UInt16 SizeOfOptionalHeader;
+            public UInt16 Characteristics;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct IMAGE_DATA_DIRECTORY
+        {
+            public UInt32 VirtualAddress;
+            public UInt32 Size;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct IMAGE_OPTIONAL_HEADER64
+        {
+            [FieldOffset(0)]
+            public ushort Magic;
+
+            [FieldOffset(2)]
+            public byte MajorLinkerVersion;
+
+            [FieldOffset(3)]
+            public byte MinorLinkerVersion;
+
+            [FieldOffset(4)]
+            public uint SizeOfCode;
+
+            [FieldOffset(8)]
+            public uint SizeOfInitializedData;
+
+            [FieldOffset(12)]
+            public uint SizeOfUninitializedData;
+
+            [FieldOffset(16)]
+            public uint AddressOfEntryPoint;
+
+            [FieldOffset(20)]
+            public uint BaseOfCode;
+
+            [FieldOffset(24)]
+            public ulong ImageBase;
+
+            [FieldOffset(32)]
+            public uint SectionAlignment;
+
+            [FieldOffset(36)]
+            public uint FileAlignment;
+
+            [FieldOffset(40)]
+            public ushort MajorOperatingSystemVersion;
+
+            [FieldOffset(42)]
+            public ushort MinorOperatingSystemVersion;
+
+            [FieldOffset(44)]
+            public ushort MajorImageVersion;
+
+            [FieldOffset(46)]
+            public ushort MinorImageVersion;
+
+            [FieldOffset(48)]
+            public ushort MajorSubsystemVersion;
+
+            [FieldOffset(50)]
+            public ushort MinorSubsystemVersion;
+
+            [FieldOffset(52)]
+            public uint Win32VersionValue;
+
+            [FieldOffset(56)]
+            public uint SizeOfImage;
+
+            [FieldOffset(60)]
+            public uint SizeOfHeaders;
+
+            [FieldOffset(64)]
+            public uint CheckSum;
+
+            [FieldOffset(68)]
+            public ushort Subsystem;
+
+            [FieldOffset(70)]
+            public ushort DllCharacteristics;
+
+            [FieldOffset(72)]
+            public ulong SizeOfStackReserve;
+
+            [FieldOffset(80)]
+            public ulong SizeOfStackCommit;
+
+            [FieldOffset(88)]
+            public ulong SizeOfHeapReserve;
+
+            [FieldOffset(96)]
+            public ulong SizeOfHeapCommit;
+
+            [FieldOffset(104)]
+            public uint LoaderFlags;
+
+            [FieldOffset(108)]
+            public uint NumberOfRvaAndSizes;
+
+            [FieldOffset(112)]
+            public IMAGE_DATA_DIRECTORY ExportTable;
+
+            [FieldOffset(120)]
+            public IMAGE_DATA_DIRECTORY ImportTable;
+
+            [FieldOffset(128)]
+            public IMAGE_DATA_DIRECTORY ResourceTable;
+
+            [FieldOffset(136)]
+            public IMAGE_DATA_DIRECTORY ExceptionTable;
+
+            [FieldOffset(144)]
+            public IMAGE_DATA_DIRECTORY CertificateTable;
+
+            [FieldOffset(152)]
+            public IMAGE_DATA_DIRECTORY BaseRelocationTable;
+
+            [FieldOffset(160)]
+            public IMAGE_DATA_DIRECTORY Debug;
+
+            [FieldOffset(168)]
+            public IMAGE_DATA_DIRECTORY Architecture;
+
+            [FieldOffset(176)]
+            public IMAGE_DATA_DIRECTORY GlobalPtr;
+
+            [FieldOffset(184)]
+            public IMAGE_DATA_DIRECTORY TLSTable;
+
+            [FieldOffset(192)]
+            public IMAGE_DATA_DIRECTORY LoadConfigTable;
+
+            [FieldOffset(200)]
+            public IMAGE_DATA_DIRECTORY BoundImport;
+
+            [FieldOffset(208)]
+            public IMAGE_DATA_DIRECTORY IAT;
+
+            [FieldOffset(216)]
+            public IMAGE_DATA_DIRECTORY DelayImportDescriptor;
+
+            [FieldOffset(224)]
+            public IMAGE_DATA_DIRECTORY CLRRuntimeHeader;
+
+            [FieldOffset(232)]
+            public IMAGE_DATA_DIRECTORY Reserved;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct IMAGE_NT_HEADERS64
+        {
+            [FieldOffset(0)]
+            public UInt32 Signature;
+
+            [FieldOffset(4)]
+            public IMAGE_FILE_HEADER FileHeader;
+
+            [FieldOffset(24)]
+            public IMAGE_OPTIONAL_HEADER64 OptionalHeader;
+
+            private string _Signature
+            {
+                get
+                {
+                    byte[] b = BitConverter.GetBytes(Signature);
+                    return System.Text.Encoding.ASCII.GetString(b);
+                }
+            }
+
+            public bool isValid
+            {
+                get { return _Signature == "PE\0\0" && OptionalHeader.Magic == 0x20b; }
+            }
         }
     }
 }
--- peruns-fart/peruns-fart/Program.cs
+++ peruns-fart/peruns-fart/Program.cs
@@ -19,27 +19,85 @@ namespace PerunsFart
 
             if(success)
             {
-                Debug("[+] New process created. Pid:{0}", new string[] { pi.dwProcessId.ToString() });
+                Debug("[+] New process created. Pid: {0}", new string[] { pi.dwProcessId.ToString() });
 
+                uint modSize = GetModuleSize("ntdll");
 
-
-
-
+                if (modSize == 0)
+                {
+                    Debug("[!] Getting NTDLL Module Size failed!");
+                }
+                else
+                {
+                    Debug("[!] ntdll Module Size: {0}", new string[] { modSize.ToString() });
+                }
 
                 // kill the spawned process
-                Debug("[+] Killing process. Pid:{0}", new string[] { pi.dwProcessId.ToString() });
+                Debug("[+] Killing process. Pid: {0}", new string[] { pi.dwProcessId.ToString() });
                 Process.GetProcessById(pi.dwProcessId).Kill();
+            }
+            else
+            {
+                Debug("[!] Unable to create a new process!");
+            }
+        }
+
+        
+        static uint GetModuleSize(string module)
+        {
+            Debug("[+] Getting Module Size: {0}", new string[] { module });
+
+            // get the size of the ntdll in memory
+            IntPtr addrMod = GetModuleBaseAddress(module);
+
+            if (addrMod != IntPtr.Zero)
+            {
+                Debug("[+] Got Module base address: 0x{0}", new string[] { addrMod.ToString("X") });
+            }
+            else
+            {
+                Debug("[!] Unable to get Module base address!");
+                return 0;
             }
 
 
+            // Marshall a pointer into an IMAGE_DOS_HEADER struct
+            IMAGE_DOS_HEADER dosHdr = (IMAGE_DOS_HEADER)Marshal.PtrToStructure(addrMod, typeof(IMAGE_DOS_HEADER));
 
+            if (dosHdr.isValid)
+            {
+                Debug("[+] Module is a valid image.");
+            }
+            else
+            {
+                Debug("[!] Module is NOT a valid image!");
+                return 0;
+            }
 
+            // get the address of the IMAGE_NT_HEADERS
+            IntPtr pNtHeaders = addrMod + dosHdr.e_lfanew;
 
+            // Marshall a pointer into an IMAGE_NT_HEADERS64 struct
+            IMAGE_NT_HEADERS64 ntHdrs = (IMAGE_NT_HEADERS64)Marshal.PtrToStructure(pNtHeaders, typeof(IMAGE_NT_HEADERS64));
 
-            
+            Debug("[+] e_lfanew equals 0x{0}", new string[] { dosHdr.e_lfanew.ToString("X") });
+            Debug("[+] NT_HEADERS address is 0x{0}", new string[] { pNtHeaders.ToString("X") });
+
+            if (ntHdrs.isValid)
+            {
+                Debug("[+] Module NT Headers is valid.");
+            }
+            else
+            {
+                Debug("[!] Module NT Headers is NOT valid!");
+                return 0;
+            }
+
+            // get the size of the NTDLL module in memory
+            uint modSize = ntHdrs.OptionalHeader.SizeOfImage;
+
+            return modSize;
         }
-
-
 
         static IntPtr GetModuleBaseAddress(string name)
         {
