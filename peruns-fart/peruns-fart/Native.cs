b16897b9eb45f75a6e1ef52afd73af35da41357d
plackyhacker <john.tear@mac.com>
2021-12-24 14:59:43 +0000

    Created a process in suspended state

--- peruns-fart/peruns-fart/Native.cs
+++ peruns-fart/peruns-fart/Native.cs
@@ -3,7 +3,72 @@ using System.Runtime.InteropServices;
 
 namespace PerunsFart
 {
-    class Native
+    public class Native
     {
+
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandles, CreateProcessFlags dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public unsafe byte* lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [Flags]
+        public enum CreateProcessFlags
+        {
+            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_NO_WINDOW = 0x08000000,
+            CREATE_PROTECTED_PROCESS = 0x00040000,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_SHARED_WOW_VDM = 0x00001000,
+            CREATE_SUSPENDED = 0x00000004,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+            DEBUG_PROCESS = 0x00000001,
+            DETACHED_PROCESS = 0x00000008,
+            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+            INHERIT_PARENT_AFFINITY = 0x00010000
+        }
     }
 }
--- peruns-fart/peruns-fart/Program.cs
+++ peruns-fart/peruns-fart/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 using static PerunsFart.Native;
@@ -9,6 +10,66 @@ namespace PerunsFart
     {
         static void Main(string[] args)
         {
+            STARTUPINFO si = new STARTUPINFO();
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            SECURITY_ATTRIBUTES pAttr = new SECURITY_ATTRIBUTES();
+            SECURITY_ATTRIBUTES tAttr = new SECURITY_ATTRIBUTES();
+
+            bool success = CreateProcess(null, "cmd.exe", ref pAttr, ref tAttr, false, CreateProcessFlags.CREATE_NEW_CONSOLE | CreateProcessFlags.CREATE_SUSPENDED, IntPtr.Zero, "C:\\Windows\\System32\\", ref si, out pi);
+
+            if(success)
+            {
+                Debug("[+] New process created. Pid:{0}", new string[] { pi.dwProcessId.ToString() });
+
+
+
+
+
+
+                // kill the spawned process
+                Debug("[+] Killing process. Pid:{0}", new string[] { pi.dwProcessId.ToString() });
+                Process.GetProcessById(pi.dwProcessId).Kill();
+            }
+
+
+
+
+
+
+            
+        }
+
+
+
+        static IntPtr GetModuleBaseAddress(string name)
+        {
+            // grab the current process
+            Process hProc = Process.GetCurrentProcess();
+
+            // find the module we are looking for
+            foreach (ProcessModule m in hProc.Modules)
+            {
+                // return the base address
+                if (m.ModuleName.ToUpper().StartsWith(name.ToUpper()))
+                    return m.BaseAddress;
+            }
+
+            // we can't find the base address
+            return IntPtr.Zero;
+        }
+
+        public static void Debug(string text, string[] args)
+        {
+#if DEBUG
+            Console.WriteLine(text, args);
+#endif
+        }
+
+        public static void Debug(string text)
+        {
+#if DEBUG
+            Console.WriteLine(text, new string[] { });
+#endif
         }
     }
 }
